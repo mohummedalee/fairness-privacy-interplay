edfcfe6f6ac906de0627671f49b63978d6c7dc4b
mohummedalee <mohummed.alee@gmail.com>
2024-06-05 14:50:45 -0400

    gosh almost deleted the whole function. train_private is here now

--- src/train.py
+++ src/train.py
@@ -9,13 +9,15 @@ import numpy as np
 from tqdm.auto import tqdm
 
 import torch
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
+import datasets
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers import RobertaTokenizer, RobertaForSequenceClassification
 from transformers import get_scheduler
-import datasets
+from private_transformers import PrivacyEngine
 
 import wandb; WANDB_PROJECT="fairness-privacy"
 
@@ -27,7 +29,18 @@ def tokenize(batch, tokenizer, maxlen):
 
 def evaluate_model(
         model: AutoModelForSequenceClassification,
-        val_dataloader: DataLoader):    
+        val_dataloader: DataLoader
+    ) -> tuple[float, float]:
+    """
+    Evaluates model on validation data
+
+    Parameters:
+    model: fine-tuned classification model
+    val_dataloader: dataloader on the validation data    
+
+    Returns:
+    tuple of (validation_loss, validation_accuracy)
+    """
     device = model.device
     model.eval()  # switch to evaluation mode
     val_loss, val_acc = 0, 0
@@ -59,6 +72,124 @@ def evaluate_model(
 #     logits, labels = eval_pred
 #     predictions = np.argmax(logits, axis=-1)
 #     return metric.compute(predictions=predictions, references=labels)
+
+
+def train_private(
+        model: AutoModelForSequenceClassification,
+        args: argparse.Namespace,
+        train_data: datasets.Dataset,
+        val_data: datasets.Dataset
+    ) -> AutoModelForSequenceClassification:
+    """
+    Finetunes classification model using a differential privacy (private-transformers, built on Opacus).
+
+    Parameters:
+    model: base model to fine-tune
+    args: parsed arguments from command line
+    train_data: tokenized train split of the data as a Dataset object
+    val_data: tokenized val split of the data as a Dataset object    
+
+    Returns:
+    fine-tuned model that can be saved
+    """
+    wandb.init(project=WANDB_PROJECT, job_type='train-custom', config=vars(args))
+
+    num_epochs = args.epochs
+    train_dataloader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_data, batch_size=args.batch_size, shuffle=True)
+    num_training_steps = num_epochs * len(train_dataloader)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    scheduler = get_scheduler(
+        name=args.lr_scheduler,
+        optimizer=optimizer,
+        num_warmup_steps=int(args.warmup_ratio * num_training_steps),
+        num_training_steps=num_training_steps
+    )    
+    privacy_engine = PrivacyEngine(
+        model,
+        batch_size=args.batch_size,
+        sample_size=len(train_dataloader.dataset),
+        epochs=num_epochs,
+        max_grad_norm=args.priv_max_grad_norm,
+        target_epsilon=args.priv_epsilon,
+    )
+    privacy_engine.attach(optimizer)
+    """
+    --- Understanding the PrivacyEngine parameters ---
+    From Opacus docs: https://opacus.ai/api/_modules/opacus/privacy_engine.html#PrivacyEngine.make_private
+    `noise_multiplier`: The ratio of the standard deviation of the Gaussian noise to
+        the L2-sensitivity of the function to which the noise is added
+        (How much noise to add)
+    `max_grad_norm`: The maximum norm of the per-sample gradients. Any gradient with norm
+        higher than this will be clipped to this value.    
+    
+    >> parameters that we want to understand better: `sample_size` <<
+        - `sample_size` and its relation to `batch_size`
+        I currently believe sample_size is L and batch_size is N in the DP-SGD algorithm
+    """
+
+    model.to(device).train()  # set to training mode
+    progress_bar = tqdm(range(num_training_steps))
+    steps = 0  # steps taken so far
+    best_val_acc = -float('inf')
+    best_model = model
+    for epoch in range(num_epochs):
+        train_loss, pts_seen, train_correct = 0, 0, 0
+        # From Opacus documentation: "experiment with multiple combinations of `batch_size` and `noise_multiplier`
+        # to find the one that provides the best possible quality at acceptable privacy guarantee."
+        for batch in train_dataloader:
+            batch_topass = {
+                'input_ids': batch['input_ids'].to(device),
+                'attention_mask': batch['attention_mask'].to(device),
+                'labels': batch['label'].to(device)
+            }
+            outputs = model(**batch_topass)
+            logits = outputs.logits
+            # count denominator of mean loss / accuracy as you go
+            pts_seen += batch_topass['input_ids'].shape[0]
+                        
+            # DEBUG: compare if these are the same
+            loss = outputs.loss
+            loss = F.cross_entropy(outputs.logits, batch['labels'].to(device), reduction="none")
+            pdb.set_trace()            
+            train_loss += loss.item() * batch_topass['input_ids'].shape[0]
+            
+            # make predictions for tracking train accuracy
+            preds = torch.argmax(logits, axis=-1)
+            batch_acc = torch.sum(preds.cpu() == batch['label']).item()
+            train_correct += batch_acc
+
+            optimizer.step(loss=loss)
+            scheduler.step()
+            optimizer.zero_grad()  # clear out gradients, compute new ones for next batch
+            # NOTE: possibility to use gradient accumulation steps if larger batches needed
+            progress_bar.update(1)
+            steps += 1
+
+            # track training metrics if args ask for it
+            if args.tracking == "steps" and steps % args.tracking_interval == 0:
+                progress_bar.set_description(f'Logged @ step {steps}')                
+                eval_loss, eval_acc = evaluate_model(model, val_dataloader)
+                train_loss /= pts_seen
+                train_correct /= pts_seen
+                metrics = {
+                    'training_loss': train_loss,
+                    'training_acc': train_correct,
+                    'eval_loss': eval_loss,
+                    'eval_acc': eval_acc
+                }
+                wandb.log(metrics)
+                pts_seen = 0; train_correct = 0; train_loss = 0
+
+        # check if i have the best model at the end of every epoch
+        epoch_loss, epoch_acc = evaluate_model(model, val_dataloader)
+        if epoch_acc > best_val_acc:
+            best_model = model
+            best_val_acc = epoch_acc
+
+    return best_model
 
 
 def train_custom_loop(
@@ -156,6 +287,7 @@ def train_custom_loop(
 
     return best_model
 
+
 def train_helper(
         args: argparse.Namespace,
         train_data: datasets.Dataset,
@@ -190,16 +322,20 @@ def train_helper(
     if args.train_mode == 'custom':
         model_ft = train_custom_loop(model, args, train_data_tok, val_data_tok)
         model_ft.save_pretrained(args.model_out_path)
+    elif args.train_mode == 'private':
+        model_ft = train_private(model, args, train_data_tok, val_data_tok)
+        model_ft.save_pretrained(args.model_out_path)        
     else:
         raise Exception(f'training mode `{args.train_mode}` not implemented')
     
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Fine-tune a model for given classification task.')
     parser.add_argument('--data-path', type=str, required=True)
     parser.add_argument('--train-mode', type=str, choices=['trainer', 'custom', 'private'], required=True,
                         help="`trainer` (use HF Trainer), `custom` (custom PyTorch training loop), or `private` (use private-transformers). If private, `privacy_args` also required")
+    parser.add_argument('--priv-epsilon', type=float, required=False)
+    parser.add_argument('--priv-max-grad-norm', type=float, required=False)
     parser.add_argument('--model-path', type=str, default="FacebookAI/roberta-base", required=False)
     parser.add_argument('--model-out-path', type=str, required=True)
     parser.add_argument('--seed', type=int, required=False, default=42)
